17746db54a5c9b3ba2eb12e5c8f4032e4635f9ae
Fernandasacosta <fernandasacostaa@gmail.com>
2024-10-14 20:33:26 -0300

    feat(layout): layout adjustments and search filter fix

--- packages/core/src/produto/index.ts
+++ packages/core/src/produto/index.ts
@@ -3,4 +3,5 @@ import FiltrarProdutos from "./FiltrarProdutos";
 import Precificavel from "./Precificavel";
 import Produto from  "./Produto"
 
-export type{Especificacoes, Precificavel, Produto, FiltrarProdutos}
+export type { Especificacoes, Precificavel, Produto }
+export { FiltrarProdutos }
