9e39dd28438560fb056ca31fb81fb8f09a889d7e
Fernandasacosta <fernandasacostaa@gmail.com>
2024-10-15 08:10:36 -0300

    fix: corrected Produto import from @gstore/core to @prisma/client in ProdutoController and ProdutoPrisma

--- apps/backend/src/produto/produto.controller.ts
+++ apps/backend/src/produto/produto.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ProdutoPrisma } from './produto.prisma';
-import { Produto } from '@prisma/client';
+import { Produto } from '@gstore/core';
 
 @Controller('produtos')
 export class ProdutoController {
@@ -10,14 +10,17 @@ export class ProdutoController {
   salvarProduto(@Body() produto: Produto): Promise<void> {
     return this.repo.salvar(produto);
   }
+
   @Get()
   obterProdutos(): Promise<Produto[]> {
     return this.repo.obter();
   }
+
   @Get(':id')
-  obterProduto(@Param('id') id:string): Promise<Produto>{
+  obterProduto(@Param('id') id: string): Promise<Produto> {
     return this.repo.obterPorId(+id);
   }
+
   @Delete(':id')
   excluirProduto(@Param('id') id: string): Promise<void> {
     return this.repo.excluir(+id);
